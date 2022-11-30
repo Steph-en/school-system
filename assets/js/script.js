77cb9948e7fbb75bd6e81fa81b62692330ec0510
Steph-en <appahstephen9@gmail.com>
2022-11-30 10:55:19 +0000

    sidebar and other basic components of every page is complet

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/**
+   * Landing Page
+   */
+const left = document.querySelector('.btn');
+const right = document.querySelector('.btn1');
+const container = document.querySelector('.container');
+
+// Hover Effect
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+
+// Click Effect
+left.onclick = function () {
+    this.innerHTML = "<div class='loader'></div>"
+    setTimeout(() => {
+        this.innerHTML = "<a href='./login.html' class='btn'>Login</a>"
+    }, 10000);
+}
+
+right.onclick = function () {
+    this.innerHTML = "<div class='loader'></div>"
+    setTimeout(() => {
+        this.innerHTML = "<a href='./login.html' class='btn'>Login</a>"
+    }, 10000);
+}
+
+// User objects 
